419d725c57f1e96a806d279715f26ed2b325981a
Aldo Rodriguez <aldors.it@gmail.com>
2023-06-17 12:41:15 +0200

    Migrate to parcel

--- src/js/main.js
+++ src/js/main.js
@@ -4,7 +4,3 @@ import '../scss/styles.scss'
 // Import all of Bootstrap's JS
 import * as bootstrap from 'bootstrap'
 
-import Icon from './favicon.ico';
-
-import LivingRoom from './living-room.jpg'
-
