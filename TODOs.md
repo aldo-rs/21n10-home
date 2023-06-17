00441bb0ccd9fba8b5a1e33c4097e14a8ca72136
Aldo Rodriguez <aldors.it@gmail.com>
2023-06-17 13:48:29 +0200

    Integrate Swiper

---
+++ TODOs.md
@@ -0,0 +1,7 @@
+# 17/06/2023
+
+## TODO
+
+* ~~Migrar el proyecto a parcel~~.
+* ~~Improve la barra de navegación: items más bold and hover subrayado~~.
+* ~~Integrar swiper (mobile first)~~.
--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,18 @@
-// Import our custom CSS
-import '../scss/styles.scss'
-
 // Import all of Bootstrap's JS
 import * as bootstrap from 'bootstrap'
 
+// core version + navigation, pagination modules:
+import Swiper, {Navigation, Pagination } from 'swiper';
+
+// import Swiper and modules styles
+import 'swiper/swiper-bundle.css';
+
+// init Swiper:
+const swiper = new Swiper('.swiper', {
+    // configure Swiper to use modules
+    modules: [Navigation, Pagination],
+    //autoHeight: true, //enable auto height
+    pagination: {
+        el: ".swiper-pagination",
+      },
+});
